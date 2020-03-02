9ea37cc7dbb491efc12a336863e176681e652451
Gabriel <gomes.g.r@outlook.com>
2020-03-02 09:23:31 -0300

    Criação do arquivo coin.ex
    Módulo em Elixir representando uma máquina de venda.
    * A cada R$ 0,45, um refrigerante é recebido
    * Somente valores de R$0,45, R$ 0,10 ou R$ 0,25 são aceitos
    * Caso insira mais, o valor será "comido"

---
+++ coin.ex
@@ -0,0 +1,36 @@
+defmodule Coin do
+  def receive_coin(amount) when amount <= 45 do
+    case amount do
+      45 -> IO.puts "Coca cola recebida!"
+      _ -> IO.puts("Falta mais #{45-amount} dinheiros")
+       receive_coin(send_message() + amount)
+    end
+  end
+
+  def receive_coin(amount) do
+    IO.puts "Dinheiro comido"
+  end
+
+  def send_message() do
+     valor = IO.gets("Digite uma quantidade: ")
+     |> String.trim()
+     |> String.to_integer()
+
+    case valor do
+      10 -> valor
+      45 -> valor
+      25 -> valor
+      _ -> "Inválido"
+     end    
+  end
+
+  def start() do
+    valor = send_message()
+    case valor do
+      "Inválido" -> IO.puts valor
+      _ -> receive_coin(valor)
+    end
+  end
+end
+
+Coin.start()
